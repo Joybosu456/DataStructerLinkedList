b67cde6e2b067fbc3d7ccc9c9f5cd8a26eba3c02
JOY <joybosu@gmail.com>
2022-03-06 17:00:51 +0530

    Joy-Welcome to my LinkedList Program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListSolu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int Choice,data,p;
+            
+            Console.WriteLine("Welcome to Linked List operation");
+           
+
+
+            
+        }
+    }
+}
