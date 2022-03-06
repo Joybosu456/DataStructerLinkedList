39bee8c1966c99fdd0f1307a6b5dc850e6390705
JOY <joybosu@gmail.com>
2022-03-06 17:06:22 +0530

    Joy,Create a simple Linked List

--- Program.cs
+++ Program.cs
@@ -9,10 +9,15 @@ namespace LinkedListSolu
             //int Choice,data,p;
             
             Console.WriteLine("Welcome to Linked List operation");
-           
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
 
 
-            
+
         }
     }
 }
