fe415f54e36ab0932bf39a80c62594c1dd27c969
JOY <joybosu@gmail.com>
2022-03-06 17:33:00 +0530

    Joy-inseart a element end of the list

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ namespace LinkedListSolu
 {
     class Program
     {
+        /// <summary>
+        /// UC2-Insert at last in linked list.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+
         static void Main(string[] args)
         {
-            //int Choice,data,p;
+            int Choice,data,p;
             
             Console.WriteLine("Welcome to Linked List operation");
             LinkedList list = new LinkedList();
@@ -16,6 +21,33 @@ namespace LinkedListSolu
             list.Add(70);
             list.Display();
 
+            while (true)
+            {
+                Console.WriteLine("1.enter the element to inserted");
+                Console.WriteLine("2.Display List");
+                          
+                Console.WriteLine("enter your Choice=");
+                Choice = Convert.ToInt32(Console.ReadLine());
+
+                if (Choice == 3)
+                    break;
+                switch (Choice)
+                {                   
+                    case 1:
+                        Console.WriteLine("enter the element to inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtLast(data);
+                        break;
+                    case 2:
+                        list.Display();
+                        break;
+                    default:
+                        Console.WriteLine("wrong choise");
+                        break;
+
+                }
+                Console.WriteLine("Exit");
+            }
 
 
         }
